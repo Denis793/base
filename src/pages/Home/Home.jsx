cd59ef4ee676292d7c620113e779cd623dba63a0
Denys Shevchenko <denys.shevchenko.pro@gmail.com>
2025-09-29 13:02:41 +0200

    add fonts update styles

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,6 +4,7 @@ import heroImg from '@/assets/images/hero/hero.png';
 import shapeYellow from '@/assets/images/shape/shape-01.svg';
 import shapeBlue from '@/assets/images/shape/shape-02.svg';
 import shapeWaves from '@/assets/images/shape/shape-03.svg';
+import { Button } from '@/shared/ui/Button';
 import styles from './Home.module.scss';
 
 export const Home = () => {
@@ -14,31 +15,30 @@ export const Home = () => {
           <section className={styles.hero}>
             <div className={styles.heroContent}>
               <h1 className={styles.heroTitle}>We specialize in UI/UX, Web Development, Digital Marketing.</h1>
-              <p className={styles.heroText}>
+              <p className="description">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque fringilla magna mauris. Nulla fermentum
                 viverra sem eu rhoncus consequat varius nisi quis, posuere magna.
               </p>
+
               <div className={styles.ctaGroup}>
-                <a href="#" className={styles.btnPrimary}>
-                  Get Started Now
+                <a href="tel:0123456789" className={styles.heroPhone}>
+                  Call us (0123) 456 – 789
                 </a>
-                <div>
-                  <a href="tel:0123456789" className={styles.heroPhone}>
-                    Call us (0123) 456 – 789
-                  </a>
-                  <p className={styles.heroSubtext}>For any question or concern</p>
-                </div>
+                <p className="description">For any question or concern</p>
               </div>
+              <Button variant="primary" href="#">
+                Get Started Now
+              </Button>
             </div>
 
             <div className={styles.heroVisual}>
               <div className={styles.heroImageWrapper}>
                 <img src={shape04} alt="Decorative background" className={styles.heroShape} />
+                <img src={heroImg} alt="Hero" className={styles.heroImage} />
+                <img src={shapeYellow} alt="Yellow quarter" className={styles.shapeYellow} />
+                <img src={shapeWaves} alt="Waves" className={styles.shapeWaves} />
+                <img src={shapeBlue} alt="Blue quarter" className={styles.shapeBlue} />
               </div>
-              <img src={heroImg} alt="Hero" className={styles.heroImage} />
-              <img src={shapeYellow} alt="Yellow quarter" className={styles.shapeYellow} />
-              <img src={shapeWaves} alt="Waves" className={styles.shapeWaves} />
-              <img src={shapeBlue} alt="Blue quarter" className={styles.shapeBlue} />
             </div>
           </section>
         </div>
