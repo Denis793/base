e707853eaacaa87b69b61708bac9d4a44de28ef8
Denys Shevchenko <denys.shevchenko.pro@gmail.com>
2025-10-07 18:18:31 +0200

    add footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Header } from '@/shared/layout/Header';
 import { Home } from '@/pages/Home';
+import { Footer } from './shared/layout/Footer';
 
 export default function App() {
   return (
@@ -17,7 +18,7 @@ export default function App() {
       </Routes>
 
       {/* <PartnersSection /> */}
-      {/* <Footer /> */}
+      <Footer />
     </Router>
   );
 }
