6981e147e441f1237858969c7d2805140b2b5005
Denys Shevchenko <denys.shevchenko.pro@gmail.com>
2025-10-09 18:32:33 +0200

    fix slider

---
+++ src/shared/data/testimonialsData.js
@@ -0,0 +1,28 @@
+import clientImage from '@/assets/images/testimonials/testimonial.png';
+
+export const testimonials = [
+  {
+    id: 1,
+    name: 'David Smith',
+    role: 'Founder @democompany',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In dolor diam, feugiat quis enim sed, ullamcorper semper ligula. Mauris consequat justo volutpat.',
+    image: clientImage,
+    company: 'dropcam',
+  },
+  {
+    id: 2,
+    name: 'Jane Cooper',
+    role: 'CEO @example',
+    text: 'Suspendisse potenti. Integer auctor eros non massa varius, eget dapibus nulla gravida.',
+    image: clientImage,
+    company: 'figma',
+  },
+  {
+    id: 3,
+    name: 'Michael Johnson',
+    role: 'CTO @techverse',
+    text: 'Curabitur placerat, lorem a hendrerit malesuada, massa augue convallis sapien, vel blandit leo tortor ut justo.',
+    image: clientImage,
+    company: 'hubspot',
+  },
+];
